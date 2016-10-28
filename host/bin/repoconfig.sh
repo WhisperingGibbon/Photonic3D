056696eff0be9be23730dfa4146aa45b881886f6
WGAndrew <andrew@whisperinggibbon.com>
2016-10-28 16:04:44 +0100

    Building in all the repos

--- host/bin/repoconfig.sh
+++ host/bin/repoconfig.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# store per-repo settings in here
+# store per-repo settings and variables in here
 # then use:
 # source repoconfig.sh
 # to import them to different scripts
@@ -11,6 +11,10 @@ if [ -z "$HOME" ] || [ "$HOME" == "/" ]; then
 fi
 
 DEFAULT_REPO="WhisperingGibbon/Photonic3D"
+DEV_REPO="Photocentric3D/Photonic3D-Dev"
+TESTKIT_REPO="Photocentric3D/Photonic3D"
+TESTKITDEV_REPO="WesGilster/Creation-Workshop-Host"
+
 CONFIG_PROPS="${HOME}/3dPrinters/config.properties"
 
 PHOTOCENTRIC_PORTNO=9091
